54b6ecda24a279bed1d7ac8d75a3534f60cec4fd
Will <weiyuchens@sina.com>
2023-02-12 22:14:53 +0800

    1. Updated .gitignore & generator.properties; 2. Add Movies data; 3. Updated README.md; 4. Start coding apis about apartment objects.

--- src/main/java/com/code4faster/dbssa/service/ApartmentService.java
+++ src/main/java/com/code4faster/dbssa/service/ApartmentService.java
@@ -2,8 +2,11 @@ package com.code4faster.dbssa.service;
 
 import com.code4faster.dbssa.mbg.model.Apartment;
 
+import java.util.List;
+
 public interface ApartmentService {
 
     int create(Apartment apartment);
+
 
 }
