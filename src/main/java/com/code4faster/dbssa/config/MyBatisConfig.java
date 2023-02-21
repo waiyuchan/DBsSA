d15e25669c0e725e362d28fffd66c73345b96248
Will <weiyuchens@sina.com>
2023-02-21 16:33:27 +0800

    1. Removed some unuseful comments.

--- src/main/java/com/code4faster/dbssa/config/MyBatisConfig.java
+++ src/main/java/com/code4faster/dbssa/config/MyBatisConfig.java
@@ -3,9 +3,6 @@ package com.code4faster.dbssa.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * MyBatis配置类 Created by will on 2022/05/25.
- */
 @Configuration
 @MapperScan({"com.code4faster.dbssa.mbg.mapper"})
 public class MyBatisConfig {
