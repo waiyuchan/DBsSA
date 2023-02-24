74ccce3182921df5b2cf3c77a13f64cc7461cfa3
Will <weiyuchens@sina.com>
2023-02-24 22:56:21 +0800

    1. Updated ApartmentStaffController

--- src/main/java/com/code4faster/dbssa/controller/apartment/ApartmentStaffController.java
+++ src/main/java/com/code4faster/dbssa/controller/apartment/ApartmentStaffController.java
@@ -1,4 +1,21 @@
 package com.code4faster.dbssa.controller.apartment;
 
+import com.code4faster.dbssa.common.api.ApiResponse;
+import com.code4faster.dbssa.common.api.ErrorCode;
+import com.code4faster.dbssa.pojo.dto.EnterpriseStaffRegistration;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/apartment")
 public class ApartmentStaffController {
+
+    @PostMapping
+    public ApiResponse createEnterpriseStaff(@RequestBody EnterpriseStaffRegistration enterpriseStaffRegistration) {
+        boolean success = true;
+        return (success) ? ApiResponse.success() : ApiResponse.failure(ErrorCode.USER_CREATE_FAILURE);
+    }
+
 }
